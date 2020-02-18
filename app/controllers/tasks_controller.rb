3c08f75641aa8f338e40b8979fac0326f5b6ecc9
Victor Park <victor.shpark@gmail.com>
2020-02-18 17:26:45 -0300

    Great app, sucky design tho

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -31,7 +31,9 @@ class TasksController < ApplicationController
 
   def destroy
     @task = Task.find(params[:id])
-    @task.destroy(task_params)
+    @task.destroy
+
+    redirect_to tasks_path
   end
 
   private
