a62af1272d9f689a3f20d09be9d210013ea80f60
Victor Park <victor.shpark@gmail.com>
2020-02-18 17:00:47 -0300

    Creation and saving working.

--- config/routes.rb
+++ config/routes.rb
@@ -5,20 +5,20 @@ Rails.application.routes.draw do
   # Read all tasks
   get 'tasks', to: 'tasks#index'
 
-  # Read one task
-  get 'tasks/:id', to: 'tasks#show', as: 'task'
-
   # Part 1/2 Create a task
   get 'tasks/new', to: 'tasks#new', as: 'new_task'
 
+  # Read one task
+  get 'tasks/:id', to: 'tasks#show', as: 'task'
+
   # Part 2/2 Create a task
-  post 'tasks/:id', to: 'tasks#new'
+  post 'tasks/', to: 'tasks#create'
 
   # Part 1/2 Update/edit a task
-  get 'tasks/:id', to: 'tasks#update'
+  patch 'tasks/:id', to: 'tasks#update'
 
   # Part 2/2 update a task
-  patch 'tasks/:id/edit', to: 'tasks#edit', as: 'edit_task'
+  get 'tasks/:id/edit', to: 'tasks#edit', as: 'edit_task'
 
   # Delete a task
   delete 'tasks/:id', to: 'tasks#destroy'
