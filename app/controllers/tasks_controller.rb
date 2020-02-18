09e89805484d5f1b6a09831bb8edbedf22eabf2e
Victor Park <victor.shpark@gmail.com>
2020-02-18 17:23:20 -0300

    Updating working

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -15,10 +15,23 @@ class TasksController < ApplicationController
     @task = Task.new(task_params)
     @task.save
 
-    redirect_to tasks_path
+    redirect_to task_path(@task)
+  end
+
+  def edit
+    @task = Task.find(params[:id])
   end
 
   def update
+    @task = Task.find(params[:id])
+    @task.update(task_params)
+
+    redirect_to task_path
+  end
+
+  def destroy
+    @task = Task.find(params[:id])
+    @task.destroy(task_params)
   end
 
   private
@@ -26,6 +39,6 @@ class TasksController < ApplicationController
   def task_params
     # *Strong params*: You need to *whitelist* what can be updated by the user
     # Never trust user data!
-    params.require(:task).permit(:title, :details)
+    params.require(:task).permit(:title, :details, :completed)
   end
 end
