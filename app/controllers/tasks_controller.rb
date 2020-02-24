1c8099da9dca68260146b179901f8eead381b2d3
Victor Park <victor.shpark@gmail.com>
2020-02-24 15:51:11 -0300

    aligned and finished.

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -13,9 +13,11 @@ class TasksController < ApplicationController
 
   def create
     @task = Task.new(task_params)
-    @task.save
-
-    redirect_to task_path(@task)
+    if @task.save
+      redirect_to task_path(@task)
+    else
+      render :new
+    end
   end
 
   def edit
